4b929b44adf91a4f30e869478e6abfa042406de4
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-01 21:25:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,8 @@
 # Write your code here
+magic_exit_number = 10 
 count = 0 
-while count < 3 do
+while count < 10 
+  break if count == magic_exit_number
   puts "I am the #{count}, I love to count!"
   count = count + 1 
 end
