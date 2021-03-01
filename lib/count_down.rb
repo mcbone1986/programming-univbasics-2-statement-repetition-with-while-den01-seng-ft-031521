7317ac7282c4060cef36307ae285a494c552b0ea
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-01 21:29:55 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,14 @@
 # Write your code here
+#magic_exit_number = 10 
+#count = 0 
+#while count < 10 
+#  break if count == magic_exit_number
+#  puts "I am the #{count}, I love to count!"
+#  count = count + 1 
+#end
+count = 10 
+while count >= 1 do 
+  puts count
+  count -= 1 
+end
+puts "Happy New Year!"
